49a9d3cc0a5b60fc1be5123873e9657fe4a58d5c
danxsodev <danilloxsoliveira13@gmail.com>
2023-06-19 02:13:00 -0300

    feat: creating view the customer registration and update form, and implementation of the zip code API

--- TCCVetCare/App_Start/BundleConfig.cs
+++ TCCVetCare/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace TCCVetCare
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/api").Include(
+            "~/Scripts/api-*"));
+
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender com ela. Após isso, quando você estiver
             // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ TCCVetCare/Scripts/api-viacep.js
@@ -0,0 +1,27 @@
+﻿async function searchAddressByZipCode() {
+
+    const zipCodeInput = document.getElementById('zip-code');
+    const zipCode = zipCodeInput.value;
+
+    try {
+
+        const response = await fetch(`https://viacep.com.br/ws/${zipCode}/json/`);
+        const data = await response.json();
+
+        const streetNameInput = document.getElementById('street-name');
+        const neighborhoodInput = document.getElementById('neighborhood');
+        const cityInput = document.getElementById('city');
+        const stateInput = document.getElementById('state');
+
+        streetNameInput.value = data.logradouro;
+        neighborhoodInput.value = data.bairro;
+        cityInput.value = data.localidade;
+        stateInput.value = data.uf;
+
+    } catch (error) {
+
+        console.log('Falha ao consultar o CEP:', error);
+
+    }
+
+}
